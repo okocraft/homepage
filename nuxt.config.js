bfaf7c004b26204491b0607077ccdf623df01791
Siroshun09 <siro.shun8@gmail.com>
2021-01-20 15:47:48 +0900

    [Add] favicon files and its settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,135 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'okocraft',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    titleTemplate: '%s - Okocraft',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'author',
+        name: 'author',
+        content: 'Okocraft'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Okocraft is a Minecraft Server.'
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'Okocraft'
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://okocraft.github.io'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Okocraft'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Okocraft is a Minecraft Server.'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://i.imgur.com/aSRElLR.png'
+      },
+      {
+        hid: 'og:locale',
+        property: 'og:locale',
+        content: 'ja_JP'
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'en_US'
+      },
+
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary'
+      },
+      {
+        hid: 'twitter:site',
+        property: 'twitter:site',
+        content: '@okocraft'
+      },
+      {
+        hid: 'twitter:creator',
+        property: 'twitter:creator',
+        content: '@okocraft'
+      },
+      {
+        hid: 'msapplication-TileColor',
+        name: 'msapplication-TileColor',
+        content: '#da532c'
+      },
+      {
+        hid: 'theme-color',
+        name: 'theme-color',
+        content: '#ffffff'
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest"
+      },
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#5bbad5"
+      },
+      {
+        rel: 'dns-prefetch',
+        href: 'https://fonts.googleapis.com'
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com/'
+      },
     ]
   },
 
