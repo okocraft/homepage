d0121ac15479e3e177d888f6d2e90609bed1942b
Siroshun09 <siro.shun8@gmail.com>
2021-01-20 16:14:33 +0900

    [Add] error page

---
+++ layouts/error.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="not-found-section">
+    <h1>
+      <font-awesome icon="exclamation-circle" />
+      <span>Not found!</span>
+    </h1>
+    <div v-if="error.statusCode === 404" class="error">
+      <p>指定されたリンクのページ "<code>{{ this.$route.path }}</code>" は見つかりませんでした。</p>
+      <p>The page "<code>{{ this.$route.path }}</code>" couldn't be found.</p>
+    </div>
+    <div v-else class="error">
+      <p>エラーが発生しました。</p>
+      <p>An error has been occurred.</p>
+    </div>
+    <div>
+     <p><NuxtLink to="/">トップページに戻る</NuxtLink> | <NuxtLink to="/">Return to top</NuxtLink></p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  metaInfo: {
+    title: 'Not Found',
+  },
+  props: ['error'],
+  layout: 'error'
+};
+</script>
+
+<style lang="scss" scoped>
+.not-found-section {
+  flex-direction: column;
+  align-items: center;
+  padding: 4rem;
+
+  h1 {
+    position: relative;
+    font-size: 3.0em;
+
+    >svg {
+      color: $not-found-red;
+      z-index: 1;
+      opacity: .5;
+      margin-right: 0.5em;
+    }
+
+    >span {
+      z-index: 2;
+    }
+  }
+
+  .error {
+    color: $not-found-red;
+
+    >p {
+      font-size: 1.3rem;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: $link-color;
+  }
+}
+</style>
--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -3,11 +3,13 @@ import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import {  
+  faExclamationCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import {
 } from '@fortawesome/free-brands-svg-icons';
 
 library.add(
+  faExclamationCircle,
 );
 
 Vue.component('font-awesome', FontAwesomeIcon);
