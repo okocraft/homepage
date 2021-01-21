821e91d4dac979b800e64b63793a5c27998cbb17
Siroshun09 <siro.shun8@gmail.com>
2021-01-21 15:32:26 +0900

    [Add] markdown component

---
+++ components/Markdown.vue
@@ -0,0 +1,259 @@
+<template>
+  <div>
+    <template v-if="content">
+      <transition name="fade" mode="out-in">
+        <article>
+          <component :is="content" />
+        </article>
+      </transition>
+    </template>
+    <template v-else>
+      <NotFound />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+    title: {
+        type: String,
+        required: false
+    }
+  },
+  data() {
+    return {
+      pageTitle: null,
+      content: null
+    }
+  },
+  head(){
+    return {title: this.pageTitle}
+  },
+  created() {
+    const markdown = require(`@${this.path}`)
+    this.content = markdown.vue.component
+
+    if (this.title != null) {
+        this.pageTitle = this.title
+    } else {
+        this.pageTitle = markdown.title != null ? markdown.title : this.$router.path.replace('-', ' ')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.markdown-body {
+  a {
+    color: $link-color;
+    text-decoration: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    position: relative;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    font-weight: bold;
+    line-height: 1.4;
+    cursor: text;
+  }
+
+  h1:hover a.anchor,
+  h2:hover a.anchor,
+  h3:hover a.anchor,
+  h4:hover a.anchor,
+  h5:hover a.anchor,
+  h6:hover a.anchor {
+    text-decoration: none;
+  }
+
+  h1 tt,
+  h1 code {
+    font-size: inherit;
+  }
+
+  h2 tt,
+  h2 code {
+    font-size: inherit;
+  }
+
+  h3 tt,
+  h3 code {
+    font-size: inherit;
+  }
+
+  h4 tt,
+  h4 code {
+    font-size: inherit;
+  }
+
+  h5 tt,
+  h5 code {
+    font-size: inherit;
+  }
+
+  h6 tt,
+  h6 code {
+    font-size: inherit;
+  }
+
+  h1 {
+    font-size: 2.0rem;
+    line-height: 1.75;
+    border-bottom: 1px solid #eee;
+  }
+
+  h2 {
+    font-size: 1.75rem;
+    line-height: 1.75;
+    border-bottom: 1px solid #eee;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    line-height: 1.3;
+  }
+
+  h4 {
+    font-size: 1.3rem;
+  }
+
+  h5 {
+    font-size: 1.2rem;
+  }
+
+  h6 {
+    font-size: 1.2rem;
+    color: #777;
+  }
+
+  p,
+  blockquote,
+  ul,
+  ol,
+  dl,
+  table {
+    font-size: 1.1rem;
+    margin: 0.8em 0;
+  }
+
+  li>ol,
+  li>ul {
+    margin: 0 0;
+  }
+
+  hr {
+    height: 2px;
+    padding: 0;
+    margin: 16px 0;
+    background-color: #e7e7e7;
+    border: 0 none;
+    overflow: hidden;
+    box-sizing: content-box;
+  }
+
+  li p.first {
+    display: inline-block;
+  }
+
+  ul,
+  ol {
+    padding-left: 30px;
+  }
+
+  ul:first-child,
+  ol:first-child {
+    margin-top: 0;
+  }
+
+  ul:last-child,
+  ol:last-child {
+    margin-bottom: 0;
+  }
+
+  blockquote {
+    border-left: 4px solid #dfe2e5;
+    padding: 0 15px;
+    color: #777777;
+  }
+
+  blockquote blockquote {
+    padding-right: 0;
+  }
+
+  table {
+    padding: 0;
+    word-break: initial;
+  }
+
+  table tr {
+    border-top: 1px solid #dfe2e5;
+    margin: 0;
+    padding: 0;
+  }
+
+  table tr:nth-child(2n),
+  thead {
+    background-color: #f8f8f8;
+  }
+
+  table th {
+    font-weight: bold;
+    border: 1px solid #dfe2e5;
+    border-bottom: 0;
+    margin: 0;
+    padding: 6px 13px;
+  }
+
+  table td {
+    border: 1px solid #dfe2e5;
+    margin: 0;
+    padding: 6px 13px;
+  }
+
+  table th:first-child,
+  table td:first-child {
+    margin-top: 0;
+  }
+
+  table th:last-child,
+  table td:last-child {
+    margin-bottom: 0;
+  }
+
+  .CodeMirror-lines {
+    padding-left: 4px;
+  }
+
+  .code-tooltip {
+    box-shadow: 0 1px 1px 0 rgba(0, 28, 36, .3);
+    border-top: 1px solid #eef2f2;
+  }
+
+  .md-fences,
+  code,
+  tt {
+    border: 1px solid #e7eaed;
+    background-color: #f8f8f8;
+    border-radius: 3px;
+    padding: 0;
+    padding: 2px 4px 0px 4px;
+    font-size: 0.9em;
+  }
+
+  code {
+    background-color: #f3f4f4;
+    padding: 0 2px 0 2px;
+  }
+}
+</style>
