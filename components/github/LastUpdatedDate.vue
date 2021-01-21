4b64cd3e36bb8f1a8a386354323fe037d95add43
Siroshun09 <siro.shun8@gmail.com>
2021-01-22 06:28:44 +0900

    [Add] LastUpdatedDate component

---
+++ components/github/LastUpdatedDate.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class=lastUpdated>
+    <span>
+        最終更新: {{ date }}
+    </span>
+  </div>
+</template>
+
+<script>
+import {Octokit} from "@octokit/rest";
+
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      date: null
+    }
+  },
+  created() {
+      this.getLastUpdated()
+  },
+  methods: {
+    async getLastUpdated() {
+      const octokit = new Octokit()
+
+      octokit.repos.listCommits({
+          owner: 'okocraft',
+          repo: 'homepage',
+          path: this.path,
+          page: 1,
+          per_page: 1
+        })
+        .then(({data}) => {
+          var json = data[0]
+          const dayjs = require('dayjs')
+          var isoDate = json.commit.author.date
+          this.date = dayjs(isoDate).locale('ja_jp').format('YYYY/MM/DD HH:MM')
+        })
+        .catch(error => {
+            this.date = "Unknown"
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.lastUpdated {
+    font-size: 1.0rem;
+    text-align: right;
+    color: $last-updated-date;
+}
+</style>
